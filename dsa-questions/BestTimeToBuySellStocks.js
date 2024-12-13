619c32425dbab708e3b75b8c0fa2167df91d2f6a
Shah-Shail <shailshah.officework@gmail.com>
2024-12-13 18:17:16 +0530

    solve frequently asked dsa questions problems

---
+++ dsa-questions/BestTimeToBuySellStocks.js
@@ -0,0 +1,47 @@
+// Question 5 - Best Time to Buy and Sell Stocks
+// You are given an array prices where prices[i] is the price of a given stock
+// on the ith day.
+// You want to maximize your profit by choosing a single day to buy one stock
+// and choosing a different day in the future to sell that stock.
+// Return the maximum profit, If you cannot achieve any profit, return 0.
+
+// Input: prices = [7, 1, 5, 3, 6, 4];  ----->>>>>  Output: 5;
+// Input: prices = [7, 6, 4, 3, 1];     ----->>>>>  Output: 0;
+
+/* Brut Force Solution */
+// const maxProfit = (stockPrices) => {
+//   let globalProfit = 0;
+
+//   for (let i = 0; i < stockPrices.length - 1; i++) {
+//     for (let j = i + 1; j < stockPrices.length; j++) {
+//       const currentProfit = stockPrices[j] - stockPrices[i];
+
+//       if (currentProfit > globalProfit) {
+//         globalProfit = currentProfit;
+//       }
+//     }
+//   }
+
+//   return globalProfit;
+// };
+/* Brut Force Solution */
+
+/* Solve using Greedy Algorithm */
+const maxProfit = (stockPrices) => {
+  let minStockPurchasePrice = stockPrices[0] || 0;
+  let profit = 0;
+
+  for (let i = 1; i < stockPrices.length; i++) {
+    if (stockPrices[i] < minStockPurchasePrice) {
+      minStockPurchasePrice = stockPrices[i];
+    }
+
+    profit = Math.max(profit, stockPrices[i] - minStockPurchasePrice);
+  }
+
+  return profit;
+};
+/* Solve using Greedy Algorithm */
+
+console.log("Case 1 >>>", maxProfit([7, 1, 5, 3, 6, 4]));
+console.log("Case 2 >>>", maxProfit([7, 6, 4, 3, 1]));
--- dsa-questions/TwoSum.js
+++ dsa-questions/TwoSum.js
@@ -2,5 +2,41 @@
 // Given an array of integers nums and an integer target,
 // return indices of the two numbers such that they add up to target.
 
-// Input: nums = [2,7,11,15], target = 9ffff
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0, 1]
 // Input: nums = [3, 2, 4], target = 6
+// Output: [1, 2]
+
+/* Brut Force Solution */
+// const twoSum = (numsArray, targetSum) => {
+//   for (let i = 0; i < numsArray.length - 1; i++) {
+//     for (let j = i + 1; j < numsArray.length; j++) {
+//       if (numsArray[i] + numsArray[j] === targetSum) {
+//         return [i, j];
+//       }
+//     }
+//   }
+//   return "Enter Proper Target Number";
+// };
+/* Brut Force Solution */
+
+/* Using JS Objects */
+const twoSum = (numsArray, targetSum) => {
+  let obj = {};
+
+  for (let i = 0; i < numsArray.length; i++) {
+    let n = numsArray[i];
+
+    if (obj[targetSum - n] >= 0) {
+      return [obj[targetSum - n], i];
+    } else {
+      obj[n] = i;
+    }
+  }
+
+  return "Enter Proper Target Number";
+};
+/* Using JS Objects */
+
+console.log("Case 1 :>> ", twoSum([1, 7, 4, 5], 6));
+console.log("Case 2 :>> ", twoSum([3, 2, 4], 6));
